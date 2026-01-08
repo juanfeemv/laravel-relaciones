efc442e2c5ec4e03a2cdf4da184c702108bb979c
Juanfe <menavegajuanfe2005@gmail.com>
2026-01-08 14:36:40 +0100

    si

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,15 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
-
+//Función básica de Vite para definir su configuración
+import { defineConfig } from 'vite'
+//Plugin que conecta Vite con Laravel.
+import laravel from 'laravel-vite-plugin'
+//Plugin que permite a Vite entender archivos .vue
+import vue from '@vitejs/plugin-vue'
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/js/app.js'],
             refresh: true,
         }),
-        tailwindcss(),
+        vue(),
     ],
-});
+})
